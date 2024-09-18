d8bee3454ad65dc5f526087d7b022cdd0424c3fe
Yakir <yakirhuri21@gmail.com>
2024-09-18 10:14:50 +0300

    wip

---
+++ ros2_robot_sim/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros2_robot_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yakir',
+    maintainer_email='yakirhuri21@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ros2_robot_sim_node = ros2_robot_sim.ros2_robot_sim_node:main'
+        ],
+    },
+)
